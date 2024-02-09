f9e8c86f951b2b471edd629cdf9480d4aab6e5a9
Nick Casamassa <nickabod.crane@gmail.com>
2024-02-08 17:53:17 -0800

    deleted node modules

--- README.md
+++
@@ -1,2 +0,0 @@
-# Ecommerce
-This is an example of an ecommerce site that connects to a database.
--- frontend/src/CartContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext, useContext } from "react";
-
-const CartContext = createContext();
-
-export { CartContext, useContext }
